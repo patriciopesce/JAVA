d84733284a123906398350dd0f73b92309a14462
Patricio_Enrique Candido_Pesce <patriciopesce@gmail.com>
2022-09-06 23:41:02 -0300

    VER 0

--- Scripts/check_cron.py
+++ Scripts/check_cron.py
@@ -10,4 +10,8 @@ with open(logfile) as f:
             continue
         pattern = r"USER \((\w+)\)$"
         result = re.search(pattern, line)
-        print(result[1])
+        if result is None:
+            continue
+        name = result [1]
+        usernames[name] = usernames.get(name, 0) + 1
+print(usernames)
